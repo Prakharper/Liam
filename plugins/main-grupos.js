9355f6f4d62b2cd5347aa6606cbf21c920034bff
Prakharper <158788969+Prakharper@users.noreply.github.com>
2025-01-30 20:52:59 -0400

    Actualizar main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -2,16 +2,16 @@ import fetch from 'node-fetch'
 
 let handler  = async (m, { conn, usedPrefix, command }) => {
 
-let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad oficial* 🚀
+let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad oficial* 🪷
 
-1- 『✯ ['⏤͟͞ू⃪𝗧𝗲𝗮𝗺 𝗖𝗼𝗺𝘂𝗻𝗶𝘁𝘆 𝗣𝗿𝗮𝗸𝗕𝗼𝘁⁞࿐✯』
+1- 『✯ ['⏤͟͞ू⃪𝗧𝗲𝗮𝗺 𝗖𝗼𝗺𝘂𝗻𝗶𝘁𝘆 𝗟𝗲𝘃𝗶𝗕𝗼𝘁⁞࿐✯』
 *✰* ${grupo}
 
 *─ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ*
 
 ➠ Enlace anulado? entre aquí! 
 
-[🌠] 𓆩 *Canal* ⁞✰⃔࿐:
+[🪷] 𓆩 *Canal* ⁞✰⃔࿐:
 *✰* ${channel}
 
 > ${dev}
